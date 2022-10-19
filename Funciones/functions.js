757b9b77adef510773100bee64a8b5a027fdae9e
stylevzero <114287004+stylevzero@users.noreply.github.com>
2022-10-19 06:17:10 -0300

    actualizamos funciones

--- Funciones/functions.js
+++ Funciones/functions.js
@@ -93,14 +93,110 @@ console.log(edad);
 */
 
 // Ejemplo de Referencia
+
+// Las estructuras como objetos son enviados como referencia
 let edades = [20];
 function modificador(edades){
     edades[0] = 25;
     console.log("Dentro de la función " + edades);
 }
+
+console.log("-- Ejemplo de referencia --");
 console.log(edades);
 modificador(edades);
 console.log(edades);
 
 
 // Funciones puras
+// No producen efectos secundarios
+
+let arreglo = [1,2,3];
+arreglo[0] = 2;  // Mutación
+
+let valor = 2;
+valor += 1;      // Mutación
+
+// Ejemplo de función pura
+
+let valores = [20];
+function modify(valores){
+    let copia = [...valores]; // Creamos una copia
+    copia[0] = 25;            // Modifico la copia y no el valor original
+    return copia; 
+}
+
+console.log("-- Funciones puras ejemplo --");
+console.log(valores);
+modify(valores);
+console.log(valores);
+
+// First class objects
+
+// Un tipo de dato puede ser asignado a una variable, retornado o enviado como argumento
+
+/* 
+Se debe poder retornar
+Se debe poder asignar a una variable
+Se debe poder enviarlo como argumento
+*/
+
+console.log("--  First class objects --");
+
+function ejecutar(funcion){
+    funcion();
+}
+
+function decirHola(){
+    console.log("Hola");
+}
+ejecutar(decirHola);
+
+function sumaTodos(){
+    let suma = 0;
+    for (let i = 0; i < arguments.length ; i++) {
+        suma += arguments[i];
+    }
+}
+
+function sumarTodosNueva(){
+    return Array.from(arguments).reduce((acc, argument) => argument + acc ,0);
+}
+
+console.log(sumarTodosNueva(1,2,3,4));
+
+function build(){
+    function a(){ console.log("Demostracion"); }
+    return a;
+}
+
+let f = build();
+
+f();
+
+
+
+console.log("-- Hoisting --");
+
+// Usar una variables y declararla después
+// Sólo se aplica con var y funciones
+
+console.log(x);
+
+var x; // undefined
+//let x;   // ReferenceError: Cannot access 'x' before initialization
+
+
+function demo() {
+    console.log(z);
+    var z;
+}
+demo();
+
+
+console.log(z);
+var z = 10;
+console.log(z);
+
+var w = 10;
+console.log(w);
+console.log(w);
