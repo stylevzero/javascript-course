a91284e21886a2d4ff3de4a4e8770aa1a300d48e
stylevzero <114287004+stylevzero@users.noreply.github.com>
2022-10-22 04:58:10 -0300

    Shorthand syntax

--- Objetos-Json/json.js
+++ Objetos-Json/json.js
@@ -9,8 +9,8 @@ let lenguaje = {
     }
 }
  // Asignamos una función a una propiedad del objeto -> Método
-console.log("Curso: " + lenguaje.nombre);
-console.log("Curso: " + lenguaje["nombre"]);
+console.log("Lenguaje: " + lenguaje.nombre);
+console.log("Lenguaje: " + lenguaje["nombre"]);
 
 console.log(lenguaje.print());
 
@@ -20,3 +20,26 @@ lenguaje["nombre"] = "Js";
 lenguaje["print"] = function() { console.log("Imprimir web")}
 
 lenguaje.print();
+
+// Shorthand syntax
+let nombre = "Javascript";
+
+//let lenguajes = { nombre: nombre};
+let lenguajes = { nombre}; 
+// Ambos son iguales, con shorthand 
+// unicamente agregando el indentificador
+// la variable (con el mismo nombre)
+
+nombre = "Go";
+console.log(lenguajes.nombre);
+console.log(nombre);
+
+lenguajes = {
+    nombre,
+    //saludar: function(){
+    saludar(){        // Shorthand syntax
+        return "Hello world";
+    }
+}
+
+console.log(lenguajes.saludar());
