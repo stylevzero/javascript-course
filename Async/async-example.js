e3f7723d323ae4095de74c80f2a8196d3678f304
stylevzero <114287004+stylevzero@users.noreply.github.com>
2022-11-14 08:21:33 -0300

    Async

--- Async/async-example.js
+++ Async/async-example.js
@@ -1,13 +1,16 @@
 
 // Async, 
-// siempre retorna una promesa 
+// siempre retorna una promesa !!
 // sin importar el cuerpo del código de la función
-// o el valor actual que retorne
+// o el valor actual que retornen
 
-async function add(){
+// Ejemplo 1
+async function add(valor1, valor2){
+    // return Promise.resolve(valor1 + valor2); //Internamente hace esto
     return valor1 + valor2; 
 }
 
+// Ejemplo 2
 async function calcular(){
     // Con async puedo usar dentro de la función await
     return new Promise((resolve, reject) => {
