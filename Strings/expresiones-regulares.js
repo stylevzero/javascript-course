cdd101ba668ced22ecc3c41a4ff2f227ef8186b8
stylevzero <114287004+stylevzero@users.noreply.github.com>
2022-11-26 09:30:18 -0300

    expresiones regulares

---
+++ Strings/expresiones-regulares.js
@@ -0,0 +1,21 @@
+
+//    /hola/
+
+// Literales, especiales (metacaracteres)
+// Tipo caracter,
+// las aserciones,
+// los agrupamientos y rangos, 
+// los cuantificadores y los unicode property escapes
+
+// Ejemplo identificar correo electronico
+
+// https://regex101.com/ para testear
+
+// /.@./ -> cualquier cantidad de caracteres @ cualquier caracteres
+// /.*@.*/ -> 0 o cualquier cantidad de caracteres @ cualquier cantidad caracteres
+// .{1,}@.{1,} / -> 0 o cualquier cantidad de caracteres @ cualquier cantidad caracteres
+
+// Buscar un texto que termine en .com
+// .{1,}\.com
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions#using_regular_expressions_in_javascript
