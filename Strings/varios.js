5d067286a99abefd16b876fe56f7f13743cf3adc
stylevzero <114287004+stylevzero@users.noreply.github.com>
2022-11-26 07:12:35 -0300

    varios

---
+++ Strings/varios.js
@@ -0,0 +1,10 @@
+// trim
+
+let texto = "   Hola mundo";
+
+console.log(texto.trim());
+console.log(texto.trimStart());
+console.log(texto.trimEnd());
+console.log(texto.length);
+console.log(texto.repeat(3));
+console.log(texto.replace("mundo", "world"));
